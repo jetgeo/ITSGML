dcb3fe4e56e9a398564bae0a3aaf4983c75e05a2
jetgeo <knut@jetlundweb.no>
2018-10-15 15:48:01 +0200

    First version of XSDs

---
+++ runshapechange.bat
@@ -0,0 +1,2 @@
+"java" -Dfile.encoding=UTF-8 -Dhttp.proxyHost=proxy.vegvesen.no -Dhttp.proxyPort=8080 -jar "C:\Program Files (x86)\Arkitektum AS\ShapeChange\ShapeChange.jar" -c "C:\DATA\GitHub\ITSGML\config\ShapeChangeConfiguration.xml"
+Pause
