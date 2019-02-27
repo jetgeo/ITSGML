bbca7d7ec5052f3954fa9f0822e3b8bda5f7272a
jetgeo <knut@jetlundweb.no>
2019-02-27 08:39:34 +0100

    After review

--- runshapechange.bat
+++ runshapechange.bat
@@ -1,2 +1,2 @@
-"java" -Dfile.encoding=UTF-8 -Dhttp.proxyHost=proxy.vegvesen.no -Dhttp.proxyPort=8080 -jar "C:\Program Files (x86)\Arkitektum AS\ShapeChange\ShapeChange.jar" -c "C:\DATA\GitHub\ITSGML\config\ShapeChangeConfiguration.xml"
+"java" -Dfile.encoding=UTF-8 -Dhttp.proxyHost=proxy.vegvesen.no -Dhttp.proxyPort=8080 -jar "C:\DATA\ShapeChange-2.7.0\ShapeChange-2.7.0.jar" -c "C:\DATA\GitHub\ITSGML\config\ShapeChangeConfiguration.xml"
 Pause
